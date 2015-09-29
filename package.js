cd55adc425b74eb71574d5770d3e9c55e2ddf29d
Alex Frazer <alexadfrazer@gmail.com>
2015-09-29 10:07:38 -0400

    A few fixes, but there are still some problems... gotta fix an error handler

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'insightfil:galil',
-  version: '0.2.5',
+  version: '0.2.6',
   summary: 'Interact with the Galil controller',
   git: 'https://github.com/artaic/Galil',
   documentation: 'README.md'
