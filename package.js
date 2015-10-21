2b66a3e86eeebb15a85296016fbbe14f17d0f825
Alex Frazer <alexadfrazer@gmail.com>
2015-10-21 12:53:15 -0400

    updates to asynchronous flow

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'insightfil:galil',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'Interact with the Galil controller',
   git: 'https://github.com/artaic/Galil',
   documentation: 'README.md'
