6e2ae1690f19834b68dd9b0fd261fd40de193bf2
Alex Frazer <alexadfrazer@gmail.com>
2016-02-18 14:46:15 -0500

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'insightfil:galil',
-  version: '1.2.0',
+  version: '1.3.1',
   summary: 'Interact with the Galil controller',
   git: 'https://github.com/artaic/Galil',
   documentation: 'README.md'
