0c796fb5486a40d8c5eef3ab36679a1053676554
Alex Frazer <alexadfrazer@gmail.com>
2016-02-19 15:22:49 -0500

    some more fixes

--- example/leaderboard.js
+++ example/leaderboard.js
@@ -1,11 +1,29 @@
 if (Meteor.isServer) {
-  s = new GalilServer();
-  Meteor.startup(function () {
-    s.clients.forEach(client => {
-      client.destroy();
-    });
-    s.listen(8124, Meteor.bindEnvironment(() => {
-      Galil.connect(s.address());
-    }));
+  galil = new Galil();
+
+  galil.onConnect(function () {
+    console.log('Connected!');
+  });
+  galil.connect(23, "169.254.248.225");
+
+  Meteor.methods({
+    'fail': function (timeToWait) {
+      check(timeToWait, Number);
+      galil.execute('cat', /^I waited 5 seconds$/, timeToWait);
+    }
+  });
+} else if (Meteor.isClient) {
+  Session.setDefault('time', 1000);
+  Template.Layout.events({
+    'click button': function () {
+      Meteor.call('fail', Session.get('time'), function (err, res) {
+        if (err) {
+          console.warn("Error has occured");
+          console.warn(err.message);
+        } else {
+          console.log('No problem');
+        }
+      })
+    }
   });
 }
