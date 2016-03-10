28b2be730b0aa468255657e7273a21728b9902de
Alex Frazer <alexadfrazer@gmail.com>
2016-03-10 15:39:26 -0500

    basic upgrades

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'insightfil:galil',
-  version: '1.3.8',
+  version: '1.3.9',
   summary: 'Interact with the Galil controller',
   git: 'https://github.com/artaic/Galil',
   documentation: 'README.md'
